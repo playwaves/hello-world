7006e423713c576d957ad2420a7906feafa7f0f8
Abbas Razzaq <abbasjrazzaq@gmail.com>
2016-06-30 20:58:56 +0100

    initial commit

---
+++ arg_parser/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace arg_parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+
+            Console.Read();
+
+        }
+    }
+}
